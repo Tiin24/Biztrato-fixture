1774acadd3d22e7e35f8a64189e5a3987d6518e8
Tiin24 <agustiinromero2401@gmail.com>
2022-11-16 12:31:52 -0300

    ruteo

---
+++ src/Components/Face Final/F_final.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function F_final() {
+  return (
+    <div>F_final</div>
+  )
+}
+
+export default F_final
---
+++ src/Components/Face de Grupos/F_grupos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function F_grupos() {
+  return (
+    <div>F_grupos</div>
+  )
+}
+
+export default F_grupos
