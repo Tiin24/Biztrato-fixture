76181c49f2dc254f70ae2b64a3fcaa7df1971e28
Tiin24 <agustiinromero2401@gmail.com>
2022-11-16 19:55:58 -0300

    add Paises.js

---
+++ src/assets/Paises.js
@@ -0,0 +1,163 @@
+let Data = {
+    "paises": [
+        {
+            "id": 1,
+            "name": "Qatar",
+            "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Qatar.svg/1920px-Flag_of_Qatar.svg.png",
+        },
+        {
+            "id": 2,
+            "name": "Ecuador",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/1024px-Flag_of_Ecuador.svg.png",
+        },
+        {
+            "id": 3,
+            "name": "Senegal",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_Senegal.svg/1024px-Flag_of_Senegal.svg.png",
+        },
+        {
+            "id": 4,
+            "name": "Paises Bajos",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1024px-Flag_of_the_Netherlands.svg.png",
+        },
+        {
+            "id": 5,
+            "name": "Inglaterra",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1024px-Flag_of_England.svg.png",
+        },
+        {
+            "id": 6,
+            "name": "Irán",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/1024px-Flag_of_Iran.svg.png",
+        },
+        {
+            "id": 7,
+            "name": "Estados Unidos",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png",
+        },
+        {
+            "id": 8,
+            "name": "Gales",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Flag_of_Wales.svg/1024px-Flag_of_Wales.svg.png",
+        },
+        {
+            "id": 9,
+            "name": "Argentina",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1024px-Flag_of_Argentina.svg.png",
+        },
+        {
+            "id": 10,
+            "name": "Arabia Saudita",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Flag_of_Saudi_Arabia_%28type_2%29.svg/1024px-Flag_of_Saudi_Arabia_%28type_2%29.svg.png",
+        },
+        {
+            "id": 11,
+            "name": "México",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1024px-Flag_of_Mexico.svg.png",
+        },
+        {
+            "id": 12,
+            "name": "Polonia",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flag_of_Poland.svg/1024px-Flag_of_Poland.svg.png",
+        },
+        {
+            "id": 13,
+            "name": "Francia",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg/1024px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg.png",
+        },
+        {
+            "id": 14,
+            "name": "Australia",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/1280px-Flag_of_Australia_%28converted%29.svg.png",
+        },
+        {
+            "id": 15,
+            "name": "Dinamarca",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/800px-Flag_of_Denmark.svg.png",
+        },
+        {
+            "id": 16,
+            "name": "Túnez",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/1024px-Flag_of_Tunisia.svg.png",
+        },
+        {
+            "id": 17,
+            "name": "España",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1024px-Bandera_de_Espa%C3%B1a.svg.png",
+        },
+        {
+            "id": 18,
+            "name": "Costa Rica",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Costa_Rica.svg/1024px-Flag_of_Costa_Rica.svg.png",
+        },
+        {
+            "id": 19,
+            "name": "Alemania",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/1024px-Flag_of_Germany.svg.png",
+        },
+        {
+            "id": 20,
+            "name": "Japón",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/1024px-Flag_of_Japan.svg.png",
+        },
+        {
+            "id": 21,
+            "name": "Bélgica",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Belgium_%28civil%29.svg/1024px-Flag_of_Belgium_%28civil%29.svg.png",
+        },
+        {
+            "id": 22,
+            "name": "Canadá",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1280px-Flag_of_Canada_%28Pantone%29.svg.png",
+        },
+        {
+            "id": 23,
+            "name": "Marruecos",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/1024px-Flag_of_Morocco.svg.png",
+        },
+        {
+            "id": 24,
+            "name": "Croacia",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/1280px-Flag_of_Croatia.svg.png",
+        },
+        {
+            "id": 25,
+            "name": "Brasil",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/800px-Flag_of_Brazil.svg.png",
+        },
+        {
+            "id": 26,
+            "name": "Serbia",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Flag_of_Serbia.svg/1024px-Flag_of_Serbia.svg.png",
+        },
+        {
+            "id": 27,
+            "name": "Suiza",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/800px-Flag_of_Switzerland.svg.png",
+        },
+        {
+            "id": 28,
+            "name": "Camerún",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Flag_of_Cameroon.svg/1024px-Flag_of_Cameroon.svg.png",
+        },
+        {
+            "id": 29,
+            "name": "Portugal",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1024px-Flag_of_Portugal.svg.png",
+        },
+        {
+            "id": 30,
+            "name": "Ghana",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Ghana.svg/1024px-Flag_of_Ghana.svg.png",
+        },
+        {
+            "id": 31,
+            "name": "Uruguay",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/1024px-Flag_of_Uruguay.svg.png",
+        },
+        {
+            "id": 32,
+            "name": "Corea del Sur",
+            "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/1024px-Flag_of_South_Korea.svg.png",
+        },
+]}
