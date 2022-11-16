d459205236e68f8519ff057d0d44ac80c8231b6f
Tiin24 <agustiinromero2401@gmail.com>
2022-11-16 18:40:45 -0300

    se creo la landing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Landing from "./Components/Landing/Landing";
 import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Landing/>} />
       </Routes>
     </BrowserRouter>
   );
