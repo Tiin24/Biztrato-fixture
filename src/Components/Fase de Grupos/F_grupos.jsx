e6d97e480da9d3ffe8b31931d0eeb931f5dfb06f
Daniel <dny.velasquez@gmail.com>
2022-11-16 19:30:03 -0500

    commit

--- src/Components/Fase de Grupos/F_grupos.jsx
+++ src/Components/Fase de Grupos/F_grupos.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 function F_grupos() {
   return (
-    <div>F_grupos</div>
+<Tabs
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="Grupo A" title="Grupo A"></Tab>
+      <Tab eventKey="Grupo B" title="Grupo B"></Tab>
+      <Tab eventKey="Grupo C" title="Grupo C"></Tab>
+      <Tab eventKey="Grupo D" title="Grupo D"></Tab>
+      <Tab eventKey="Grupo E" title="Grupo E"></Tab>
+      <Tab eventKey="Grupo F" title="Grupo F"></Tab>
+      <Tab eventKey="Grupo G" title="Grupo G"></Tab>
+      <Tab eventKey="Grupo h" title="Grupo H"></Tab>
+    </Tabs>
   )
 }
 
