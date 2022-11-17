265767d941b16a1a6a658a32614c3c9ab6d5a645
Tiin24 <agustiinromero2401@gmail.com>
2022-11-17 12:50:49 -0300

    se maqueto toda la face se grupos falta las puntuaciones

--- src/Components/Fase de Grupos/F_grupos.jsx
+++ src/Components/Fase de Grupos/F_grupos.jsx
@@ -9,14 +9,278 @@ function F_grupos() {
       id="uncontrolled-tab-example"
       className="mb-3"
     >
-      <Tab eventKey="Grupo A" title="Grupo A"></Tab>
-      <Tab eventKey="Grupo B" title="Grupo B"></Tab>
-      <Tab eventKey="Grupo C" title="Grupo C"></Tab>
-      <Tab eventKey="Grupo D" title="Grupo D"></Tab>
-      <Tab eventKey="Grupo E" title="Grupo E"></Tab>
-      <Tab eventKey="Grupo F" title="Grupo F"></Tab>
-      <Tab eventKey="Grupo G" title="Grupo G"></Tab>
-      <Tab eventKey="Grupo h" title="Grupo H"></Tab>
+      <Tab eventKey="Grupo A" title="Grupo A">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Qatar</h1><input type="number" /></div>
+            <div className='pais2'><h1>Ecuador</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Senegal</h1><input type="number" /></div>
+            <div className='pais2'><h1>P.Bajos</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>P.Bajos</h1><input type="number"/></div>
+            <div className='pais2'><h1>Ecuador</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Qatar</h1><input type="number" /></div>
+            <div className='pais2'><h1>Senegal</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>P.Bajos</h1><input type="number"/></div>
+            <div className='pais2'><h1>Qatar</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Ecuador</h1><input type="number"/></div>
+            <div className='pais2'><h1>Senegal</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo B" title="Grupo B">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Inglaterra</h1><input type="number" /></div>
+            <div className='pais2'><h1>Iran</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+          <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>EE.UU</h1><input type="number" /></div>
+            <div className='pais2'><h1>Gales</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Inglaterra</h1><input type="number"/></div>
+            <div className='pais2'><h1>EE.UU</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Gales</h1><input type="number" /></div>
+            <div className='pais2'><h1>Iran</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Iran</h1><input type="number"/></div>
+            <div className='pais2'><h1>EE.UU</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Gales</h1><input type="number"/></div>
+            <div className='pais2'><h1>Inglaterra</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo C" title="Grupo C">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Argentina</h1><input type="number" /></div>
+            <div className='pais2'><h1>Arabia Saudita</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Mexico</h1><input type="number" /></div>
+            <div className='pais2'><h1>Polonia</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Argentina</h1><input type="number"/></div>
+            <div className='pais2'><h1>Mexico</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Polonia</h1><input type="number" /></div>
+            <div className='pais2'><h1>Arabia Saudita</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Arabia Saudita</h1><input type="number"/></div>
+            <div className='pais2'><h1>Mexico</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Polonia</h1><input type="number"/></div>
+            <div className='pais2'><h1>Argentina</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo D" title="Grupo D">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Dinamarca</h1><input type="number" /></div>
+            <div className='pais2'><h1>Túnez</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Francia</h1><input type="number" /></div>
+            <div className='pais2'><h1>Australia</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Túnez</h1><input type="number"/></div>
+            <div className='pais2'><h1>Australia</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Francia</h1><input type="number"/></div>
+            <div className='pais2'><h1>Dinamarca</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Túnez</h1><input type="number"/></div>
+            <div className='pais2'><h1>Francia</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Australia</h1><input type="number"/></div>
+            <div className='pais2'><h1>Dinamarca</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo E" title="Grupo E">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Alemania</h1><input type="number" /></div>
+            <div className='pais2'><h1>Japón</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>España</h1><input type="number" /></div>
+            <div className='pais2'><h1>Costa Rica</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Japón</h1><input type="number"/></div>
+            <div className='pais2'><h1>Costa Rica</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>España</h1><input type="number"/></div>
+            <div className='pais2'><h1>Alemania</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Costa Rica</h1><input type="number"/></div>
+            <div className='pais2'><h1>Alemania</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Japón</h1><input type="number"/></div>
+            <div className='pais2'><h1>España</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo F" title="Grupo F">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Marruecos</h1><input type="number" /></div>
+            <div className='pais2'><h1>Croacia</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Bélgica</h1><input type="number" /></div>
+            <div className='pais2'><h1>Canadá</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Bélgica</h1><input type="number"/></div>
+            <div className='pais2'><h1>Marruecos</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Canadá</h1><input type="number"/></div>
+            <div className='pais2'><h1>Croacia</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Bélgica</h1><input type="number"/></div>
+            <div className='pais2'><h1>Croacia</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Canadá</h1><input type="number"/></div>
+            <div className='pais2'><h1>Marruecos</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo G" title="Grupo G">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Suiza</h1><input type="number" /></div>
+            <div className='pais2'><h1>Camerún</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Brasil</h1><input type="number" /></div>
+            <div className='pais2'><h1>Serbia</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Serbia</h1><input type="number"/></div>
+            <div className='pais2'><h1>Camerún</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Brasil</h1><input type="number"/></div>
+            <div className='pais2'><h1>Suiza</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Brasil</h1><input type="number"/></div>
+            <div className='pais2'><h1>Camerún</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Serbia</h1><input type="number"/></div>
+            <div className='pais2'><h1>Suiza</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
+      <Tab eventKey="Grupo h" title="Grupo H">
+        <div className='container_card'>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Uruguay</h1><input type="number" /></div>
+            <div className='pais2'><h1>Corea del sur</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Portugal</h1><input type="number" /></div>
+            <div className='pais2'><h1>Ghana</h1><input type="number"/></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Ghana</h1><input type="number"/></div>
+            <div className='pais2'><h1>Corea del sur</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Jor</p></div>
+            <div className='pais1'><h1>Portugal</h1><input type="number"/></div>
+            <div className='pais2'><h1>Uruguay</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Portugal</h1><input type="number"/></div>
+            <div className='pais2'><h1>Corea del sur</h1><input type="number" /></div>
+          </div>
+          <div className='container_countries'>
+            <div className='info'><p>Dom.20/11  {/*Horario*/}  Doha</p></div>
+            <div className='pais1'><h1>Ghana</h1><input type="number"/></div>
+            <div className='pais2'><h1>Uruguay</h1><input type="number" /></div>
+          </div>
+        </div>
+      </Tab>
     </Tabs>
   )
 }
