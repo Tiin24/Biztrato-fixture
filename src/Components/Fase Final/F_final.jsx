e6b06f0714dfbb63434343c064238fcba8845632
Tiin24 <agustiinromero2401@gmail.com>
2022-11-18 15:09:20 -0300

    full maquetado fase final

--- src/Components/Fase Final/F_final.jsx
+++ src/Components/Fase Final/F_final.jsx
@@ -13,208 +13,224 @@ function F_final() {
         </div>
         <div className={style.dynamic_fixture}>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>OCTAVOS</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>OCTAVOS</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number"/>
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>OCTAVOS</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number"/>
             </div>
           </div>
           <div className={style.dynamic_fixture__cuartos}>
-            <h2>Cuartos</h2>
             <div className='dynamic_fixture_countrie__left_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />           
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>          
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__semifinal}>
-            <h2>Semifinal</h2>
             <div className='dynamic_fixture_countrie__left_semifinal'>
+              <h6>Semifinal</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_semifinal'>
+              <h6>Semifinal</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>            
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>           
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__cuartos}>
-            <h2>Cuartos</h2>
             <div className='dynamic_fixture_countrie__left_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>            
               <input type="imagen del equipo" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>          
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>            
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__final}>
-            <h2>Final</h2>
             <div className='dynamic_fixture_countrie__left_final'>
+              <h6>Final</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_final'>
+              <h6>Final</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>            
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div> 
           </div>
           <div className={style.dynamic_fixture__tercer_cuarto}>
-            <h2>3er y 4to Puesto</h2>
             <div className='dynamic_fixture_countrie__left_tercer_cuarto'>
+              <h6>3er y 4to Puesto</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_tercer_cuarto'>
+              <h6>3er y 4to Puesto</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div> 
           </div>
           <div className={style.dynamic_fixture__cuartos}>
-            <h2>Cuartos</h2>
             <div className='dynamic_fixture_countrie__left_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>            
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>           
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__semifinal}>
-            <h2>Semifinal</h2>
             <div className='dynamic_fixture_countrie__left_semifinal'>
+              <h6>Semifinal</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>            
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_semifinal'>
+              <h6>Semifinal</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>          
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__cuartos}>
-            <h2>Cuartos</h2>
             <div className='dynamic_fixture_countrie__left_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_cuartos'>
+              <h6>Cuartos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
           </div>
           <div className={style.dynamic_fixture__octavos}>
-            <h2>Octavos</h2>
             <div className='dynamic_fixture_countrie__left_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
             </div>
             <div className='dynamic_fixture_countrie__rigth_octavos'>
+              <h6>Octavos</h6>
               <img src="imagen del equipo" alt="imagen del equipo" />
               <p>{/* Nombre del equipo */}</p>
               <input type="number" />
